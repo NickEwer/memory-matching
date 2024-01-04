c49c7ef540cbf882a73e00d2cebe9f03e28976fa
BriannaRocha <rochabrianna17@gmail.com>
2024-01-03 23:52:59 -0500

    add delay for not a match

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ const cardEls = document.querySelectorAll('.card')
 const scoreEl = document.querySelector('#score')
 const messageEl = document.querySelector('#message')
 const resetBtn = document.getElementById('reset-button')
+let countdownEl = document.getElementById('countdown')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -47,7 +48,7 @@ function setupBoard() {
   })
 }
 
-// copied shuffle Fisher-Yates function from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array#2450976
+// shuffle cards using Fisher-Yates shuffle function from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array#2450976
 
 function shuffleCards(cards) {
   let currentIndex = cards.length, randomIndex;
@@ -83,11 +84,15 @@ function flipCard (cardIdx) {
 function checkForMatch(cardIdx) {
   if (board[cardIdx].name === board[activeCard].name) {
     match ++
+    activeCard = null
   } else {
-    board[cardIdx].isFlipped = false
-    board[activeCard].isFlipped = false
+    setTimeout(() => {
+      board[cardIdx].isFlipped = false
+      board[activeCard].isFlipped = false
+      render()
+      activeCard = null
+    }, 1250) 
   }
-  activeCard = null
 }
 
 function checkForWinner() {
@@ -125,21 +130,21 @@ function render() {
 //// 3. Upon loading, the game state should be initialized, and a function should be called to render this game state
 ////  - Create an ‘init’ function
 ////  - Create 16 elements representing the cards from the deck
-//  - Create a function that will shuffle and randomize card deck
-//  - Create a score status representing how many matches scored and how many turns are left
-// 4. The state of the game should be rendered to the user
-//  - Create a render and update function to update card elements to indicate which cards have already been flipped
+////  - Create a function that will shuffle and randomize card deck
+//  - Create a score status representing how many matches scored and how many turns tried
+//// 4. The state of the game should be rendered to the user
+////  - Create a render and update function to update card elements to indicate which cards have already been flipped
 //  - Update card board, matches, score, and turns
 // 5. Define the required constants
 //   - Create a data array within its own file representing all possible cards and images used for the game
-// 6. Handle a player clicking a card with a `handleClick` function
-//  - Flip card if clicked on 
-//  - On second card click, flip card and check for a match 
-//  - Determine a match and have cards stay flipped if true and flip back down if false
+//// 6. Handle a player clicking a card with a `handleClick` function
+////  - Flip card if clicked on 
+////  - On second card click, flip card and check for a match 
+////  - Determine a match and have cards stay flipped if true and flip back down if false
 //  - Update turns and score per turn
-//  - Determine winner when all sets are matched 
+////  - Determine winner when all sets are matched 
 //  - Determine loser when turns run out and not all cards are matched
-// 7. Create Reset functionality
-//  - Add a reset button with an event listener to clear the score and call the init function
+//// 7. Create Reset functionality
+////  - Add a reset button with an event listener to clear the score and call the init function
 
 
