579b4caa24b73906bff3c7c58f12b296f24ec52a
Nick Ewer <ne@live.ca>
2024-01-06 09:54:36 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# <p align="center">**Homer's Memory Challenge!**</p>
+
+![Alt text](assets/images/GameScreenshot.png)
+
+Enjoy a Simpsons themed memory matching card game! In this game, you must pick two cards to see if they are a match. If they are not a match, you can keep trying. If you do get a match, the cards matched will stay flipped for you. Keep track of every card clicked by looking at the Move counter. You can also know your score by looking at the Matches counter for every match that is made. However, watch out! You will only have three minutes per game or it is game over... D'oh! But don't worry! There is a reset button to try again. Once all of the cards are matched with its corresponding pair, the timer will stop and you have won the game! Good luck and have fun with Homer's Memory Challenge!
+
+
+# [Play Homer's Memory Challenge here!](https://mmcg-br.netlify.app/) 🍩
+
+## [Planning Materials](https://docs.google.com/document/d/119J0NP4qZ_RGF30xsDbitaX8LVAcPrxDHGMycA0jUIQ/edit?usp=sharing) 💭
+## Technologies used 💻
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white) ![Netlify](https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7) ![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)   ![Google Drive](https://img.shields.io/badge/Google%20Drive-4285F4?style=for-the-badge&logo=googledrive&logoColor=white)
+
+## [Credits](https://docs.google.com/document/d/1DInBiYHscKJ5IsrXjgRYUO5xqDJmOcW718aeQhDqxVE/edit?usp=sharing) 👏
+
+![MDN Web Docs](https://img.shields.io/badge/MDN_Web_Docs-black?style=for-the-badge&logo=mdnwebdocs&logoColor=white) ![FreeCodeCamp](https://img.shields.io/badge/Freecodecamp-%23123.svg?&style=for-the-badge&logo=freecodecamp&logoColor=green) ![Google](https://img.shields.io/badge/google-4285F4?style=for-the-badge&logo=google&logoColor=white) ![Notion](https://img.shields.io/badge/Notion-%23000000.svg?style=for-the-badge&logo=notion&logoColor=white) ![Slack](https://img.shields.io/badge/Slack-4A154B?style=for-the-badge&logo=slack&logoColor=white)
+
+## Ice Box 🧊
+
+- [x] Font Design
+- [ ] Highlight Cards flipped
+- [x] Card Animations
+- [x] Timer
+- [x] Sound
+- [x] Confetti
+- [x] Audio Button
+- [ ] Difficulty Modes
+- [ ] Different Themes for each difficulty; e.g. Universal Studios Park themes
+- [ ] Change Confetti to Themed emotes
+- [ ] Navigation Bar
+- [ ] Dark Mode
+- [ ] Mobile Responsive
---
+++ js/app.js
@@ -0,0 +1,201 @@
+/*-------------------------------- Constants --------------------------------*/
+const cardContainer = document.querySelector(".container")
+const cardFront = document.querySelectorAll(".front")
+const cardList = ["homer.png", "marge.png", "bart.png", "lisa.png", "maggie.png", "krusty.png", "burns.png", "ned.png"]
+
+const dohSound = new Audio('../assets/audio/doh.mp3')
+const woohooSound = new Audio('../assets/audio/homer-woohoo.mp3')
+const winnerSound = new Audio('../assets/audio/omgwinner.mp3')
+const buzzerSound = new Audio('../assets/audio/timerbuzz.mp3')
+const backgroundSound = new Audio('../assets/audio/simptheme.mp3')
+dohSound.volume = 0.1
+woohooSound.volume = 0.1
+winnerSound.volume = 0.1
+buzzerSound.volume = 0.1
+backgroundSound.volume = 0.02
+backgroundSound.loop = true
+
+/*---------------------------- Variables (state) ----------------------------*/
+let board, moves, match, activeCard, mismatchShowing, winner, lose, timeLeft, timerIntervalId
+
+/*------------------------ Cached Element References ------------------------*/
+const cardEls = document.querySelectorAll('.card')
+const movesEl = document.querySelector('#moves')
+const matchEl = document.querySelector('#matches')
+const timerEl = document.querySelector('#timer')
+const messageEl = document.querySelector('#message')
+const resetBtn = document.querySelector('#reset-button')
+const musicBtn = document.querySelector('#music-button')
+
+/*----------------------------- Event Listeners -----------------------------*/
+cardEls.forEach(function(cardEl) {
+  cardEl.addEventListener('click', handleClick)
+})
+resetBtn.addEventListener('click', init)
+musicBtn.addEventListener('click', toggleMusic)
+
+/*-------------------------------- Functions --------------------------------*/
+init()
+
+function init() {
+  board = []
+  moves = 0
+  match = 0
+  activeCard = null
+  mismatchShowing = false
+  winner = false
+  lose = false
+  timeLeft = 180
+  setupBoard()
+  clearInterval(timerIntervalId)
+  timerIntervalId = null
+  cardEls.forEach(cardEl => {
+    cardEl.classList.remove('animate__animated', 'animate__flipInY')
+  })
+  render()
+}
+
+function setupBoard() {
+  let cards = cardList.concat(cardList)
+  cards = shuffleCards(cards)
+  cards.forEach((card) => {
+    let cardObj = {
+      name: card, 
+      isFlipped: false
+    }
+    board.push(cardObj)
+  })
+}
+
+// shuffle cards using Fisher-Yates shuffle function from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array#2450976
+
+function shuffleCards(cards) {
+  let currentIndex = cards.length, randomIndex;
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [cards[currentIndex], cards[randomIndex]] = [cards[randomIndex], cards[currentIndex]];
+  }
+  return cards;
+}
+
+function startTimer() {
+	if (timerIntervalId) {
+		return
+	}
+  renderTime()
+	timerIntervalId = setInterval(tick, 1000)
+}
+
+function tick() {
+	timeLeft--
+  if (timeLeft === 0) {
+    clearInterval(timerIntervalId)
+    lose = true
+    buzzerSound.play()
+    render()
+  } else {
+    renderTime()
+  }
+}
+
+function renderTime() {
+  let min = Math.floor(timeLeft / 60)
+  let sec = timeLeft % 60
+  if (sec < 10) {
+    timerEl.textContent = `${min}:0${sec}`
+  } else {
+    timerEl.textContent = `${min}:${sec}`
+  }
+}
+
+function handleClick(evt) {
+  const cardIdx = parseInt(evt.target.id.replace('card', ''))
+  if (board[cardIdx].isFlipped || mismatchShowing || lose) {
+    return 
+  }
+  if (activeCard !== null) {
+    flipCard(cardIdx)
+    checkForMatch(cardIdx)
+  } else {
+    activeCard = cardIdx
+    flipCard(cardIdx)
+  }
+  moves++
+  startTimer() 
+  checkForWinner()
+  render()
+  backgroundSound.play()
+}
+
+function flipCard (cardIdx) {
+  board[cardIdx].isFlipped = !board[cardIdx].isFlipped
+  cardEls[cardIdx].classList.add('animate__animated', 'animate__flipInY')
+}
+
+function checkForMatch(cardIdx) {
+  if (board[cardIdx].name === board[activeCard].name) {
+    match++
+    activeCard = null
+    woohooSound.play()
+  } else {
+    mismatchShowing = true
+    dohSound.play()
+    setTimeout(() => {
+      board[cardIdx].isFlipped = false
+      board[activeCard].isFlipped = false
+      render()
+      activeCard = null
+      mismatchShowing = false
+    }, 1500)
+  }
+}
+
+function checkForWinner() {
+  if (match === 8) {
+    clearInterval(timerIntervalId)
+    winner = true
+    confetti.start(3000)
+    winnerSound.play()
+  }
+}
+
+function updateBoard() {
+  board.forEach(function(card, idx) {
+    if (card.isFlipped) {
+      cardEls[idx].style.backgroundImage = `url(assets/images/${card.name})`
+    } else {
+      cardEls[idx].style.backgroundImage = "url(assets/images/cardback.png)"
+    }
+  })
+} 
+
+function updateStats() {
+  movesEl.textContent = moves
+  matchEl.textContent = match
+}
+
+function updateMessage() {
+  if (!winner && !lose){
+    messageEl.textContent = "Click a card, any card!"
+  } else if (winner) {
+    messageEl.textContent = "Woo-hoo! I am so smart! S-M-R-T!"
+  } else {
+    messageEl.textContent = "Trying is the first step towards failure!"
+  }
+}
+
+function toggleMusic() {
+  if (backgroundSound.volume === 0) {
+    backgroundSound.volume = 0.02
+  } else {
+    backgroundSound.volume = 0
+  }
+}
+
+function render() {
+  updateBoard()
+  updateStats()
+  renderTime()
+  updateMessage()
+}
